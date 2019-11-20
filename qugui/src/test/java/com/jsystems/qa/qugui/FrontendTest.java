36ced25b5ae7d607590bb3de98a7251bbd324181
llechows2000 <lechowski.lukasz@gmail.com>
2019-11-20 08:16:57 +0100

    Wordpress.com login GUI test with id and css locator

--- qugui/src/test/java/com/jsystems/qa/qugui/FrontendTest.java
+++ qugui/src/test/java/com/jsystems/qa/qugui/FrontendTest.java
@@ -3,7 +3,10 @@ package com.jsystems.qa.qugui;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static com.google.common.truth.Truth.assertThat;
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
@@ -26,5 +29,57 @@ public class FrontendTest extends ConfigFrontend {
     } catch (InterruptedException e){
       e.printStackTrace();
     }
+  }
+
+  @Test
+  public void loginTest() {
+    driver.get("https://www.wordpress.com/");
+
+    WebElement loginIcon = driver.findElement(By.cssSelector(".x-nav-item.x-nav-item--wide.x-nav-item--logged-in"));
+
+    WebDriverWait wait = new WebDriverWait(driver, 30);
+    wait.until(ExpectedConditions.elementToBeClickable(loginIcon));
+
+    loginIcon.click();
+
+    String usernameOrEmailSelector = "usernameOrEmail";
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(usernameOrEmailSelector)));
+
+    WebElement usernameInput = driver.findElement(By.id(usernameOrEmailSelector));
+
+    usernameInput.clear();
+    usernameInput.sendKeys("testautomation112019@wp.pl");
+
+    String primaryButtonSelector = ".button.form-button.is-primary";
+    wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(primaryButtonSelector)));
+    WebElement usernameButton = driver.findElement(By.cssSelector(primaryButtonSelector));
+    usernameButton.click();
+
+    wait.until(ExpectedConditions.elementToBeClickable(By.id("password")));
+    WebElement inputPassword = driver.findElement(By.id("password"));
+
+    inputPassword.clear();
+    inputPassword.sendKeys("testautomation");
+    wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(primaryButtonSelector)));
+    WebElement buttonPassword = driver.findElement(By.cssSelector(primaryButtonSelector));
+    usernameButton.click();
+
+    String userAvatarSelector = ".masterbar__item.masterbar__item-me";
+    wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(userAvatarSelector)));
+
+    WebElement userAvatar = driver.findElement(By.cssSelector(userAvatarSelector));
+    userAvatar.click();
+
+    String userDisplayNameSelector = ".profile-gravatar__user-display-name";
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(userDisplayNameSelector)));
+
+    WebElement userDisplayName = driver.findElement(By.cssSelector(userDisplayNameSelector));
+    String userDisplayNameText = userDisplayName.getText();
+
+    assertThat(userDisplayNameText).isEqualTo("testautomation112019");
+
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(primaryButtonSelector)));
+    WebElement saveUserDetailsButton = driver.findElement(By.cssSelector(primaryButtonSelector));
+    assertThat(!saveUserDetailsButton.isDisplayed());
   }
 }
