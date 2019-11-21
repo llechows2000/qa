5b23aa9f58f5493a0692f199b777939c6ca0b81b
llechows2000 <lechowski.lukasz@gmail.com>
2019-11-21 15:58:32 +0100

    Api tests

---
+++ qaapi/src/main/java/com/jsystems/qa/qaapi/Device.java
@@ -0,0 +1,14 @@
+package com.jsystems.qa.qaapi;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class Device {
+
+    @JsonProperty(required = true)
+    public String type;
+
+    @JsonProperty(value = "device.model", required = true)
+    List<DeviceModel> deviceModel;
+}
---
+++ qaapi/src/main/java/com/jsystems/qa/qaapi/DeviceModel.java
@@ -0,0 +1,12 @@
+package com.jsystems.qa.qaapi;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DeviceModel {
+
+
+    @JsonProperty(required = true)
+    public String produce;
+    @JsonProperty(value = "screen.size", required = true)
+    public double screenSize;
+}
